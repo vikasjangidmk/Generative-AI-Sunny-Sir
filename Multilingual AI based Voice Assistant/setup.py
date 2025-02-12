e9ac1bb739c9e108a95ba8aa821c6a0b0a51ce56
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2025-02-12 15:36:04 +0530

    completed

---
+++ Multilingual AI based Voice Assistant/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Gen AI Project",
+    version="0.0.0",
+    author="Vikas Jangid",
+    author_email="vikasjangidmk@gmail.com",
+    packages=find_packages()
+)
